7446eb9e2ed55fe3b4f4ee6f745ab5901042b292
parasharsmart <parasharsmart@gmail.com>
2014-05-08 16:16:18 +0530

    Initial Project

---
+++ ParasharSocialContactManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParasharSocialContactManager.Startup))]
+namespace ParasharSocialContactManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
